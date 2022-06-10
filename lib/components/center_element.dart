7b8b93f177a9145b984fd2cd644f749d73862913
grandauto3 <39314276+grandauto3@users.noreply.github.com>
2022-06-10 02:45:12 +0200

    add center element

---
+++ lib/components/center_element.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CenterElement extends StatelessWidget {
+  const CenterElement({
+    Key? key,
+    required this.children,
+  }) : super(key: key);
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: children,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,53 +1,44 @@
 import 'package:flutter/material.dart';
 import '../components/home_page_entry.dart';
+import '../components/center_element.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({
     Key? key,
   }) : super(key: key);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Card(
-          child: Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text('Portfolio'),
-                HomepageEntry(
-                  title: 'Unity Level Editor',
-                  onPressed: () {},
-                ),
-                HomepageEntry(
-                  title: 'GodArena',
-                  onPressed: () {},
-                ),
-                HomepageEntry(
-                  title: 'Tents and Trees (WIP)',
-                  onPressed: () {},
-                ),
-                HomepageEntry(
-                  title: 'Rehago',
-                  onPressed: () {},
-                ),
-                HomepageEntry(
-                  title: 'Portfolio source',
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
+    return CenterElement(
+      children: [
+        const Text('Portfolio'),
+        HomepageEntry(
+          title: 'Unity Level Editor',
+          onPressed: () {
+          },
         ),
-      ),
+        HomepageEntry(
+          title: 'GodArena',
+          onPressed: () {
+          },
+        ),
+        HomepageEntry(
+          title: 'Tents and Trees (WIP)',
+          onPressed: () {
+          },
+        ),
+        HomepageEntry(
+          title: 'Rehago',
+          onPressed: () {
+          },
+        ),
+        HomepageEntry(
+          title: 'Portfolio source',
+          onPressed: () {
+          },
+        ),
+      ],
     );
   }
 }
