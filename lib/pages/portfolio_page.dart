93e5e00c40705f53f87081ecc01259f0045a95e2
grandauto3 <39314276+grandauto3@users.noreply.github.com>
2022-06-20 17:59:02 +0200

    remove unnecassary import

--- lib/pages/portfolio_page.dart
+++ lib/pages/portfolio_page.dart
@@ -1,7 +1,6 @@
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:portfolio/pages/markdown_page.dart';
 import '../components/center_element.dart';
 import '../core/io/io_handler.dart';
 
